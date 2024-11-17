c89019e2d376cd64ca916281166de05ad5ad94a0
Nadimul Rahib <nadimulrahib38@gmail.com>
2024-11-17 00:54:49 -0800

    added a product grid systems

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,8 +5,8 @@ import UserDropDown from "./Home/UserDropDown";
 const Navbar = () => {
   const {user}=UseAuth()
   return (
-    <div>
-      <div className="navbar bg-base-100">
+    <div className="bg-green-200">
+      <div className="navbar   container mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,17 @@
-
 const Products = () => {
-    return (
-        <div>
-            <h1>Product</h1>
-        </div>
-    );
+  return (
+    <div className="container mx-auto">
+      <h1 className="my-8 text-2xl font-semibold text-center">All Products</h1>
+      <div className="flex justify-between items-center w-full mb-5">
+        <h1>Search bar</h1>
+        <h1>Sorting</h1>
+      </div>
+      <div className="grid grid-cols-12">
+        <div className="col-span-2">filterBars</div>
+        <div className="col-span-10">Products</div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
